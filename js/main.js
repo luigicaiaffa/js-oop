780400ba31161c17eee7c22e23f5c09c4c71886c
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-11-14 16:07:28 +0100

    create class

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("JS OK");
+class Veicolo {
+  marca;
+  anno;
+  colore;
+
+  constructor(marcaVeicolo, annoVeicolo, coloreVeicolo) {
+    this.marca = marcaVeicolo;
+    this.anno = annoVeicolo;
+    this.colore = coloreVeicolo;
+  }
+}
